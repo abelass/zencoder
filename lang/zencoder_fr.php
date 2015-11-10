205b9a48a81f3d8706bcbf74ac2a8d8eb753134a
Rainer <rainer@websimple.be>
2015-11-10 20:30:20 +0100

    la configuration

--- lang/zencoder_fr.php
+++ lang/zencoder_fr.php
@@ -5,16 +5,19 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// Z
-	'zencoder_titre' => 'Zencoder',
+  // A
+  'api_key' => 'API key',
 
-	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
-	'cfg_titre_parametrages' => 'Paramétrages',
+  // C
+  'cfg_exemple_explication' => 'Explication de cet exemple',
+  'cfg_titre_parametrages' => 'Paramétrages',
 
-	// T
-	'titre_page_configurer_zencoder' => 'Configurations',
+  // T
+  'titre_page_configurer_zencoder' => 'Configurations',
+  
+  // Z
+  'zencoder_titre' => 'Zencoder',
+
 );
 
 ?>
