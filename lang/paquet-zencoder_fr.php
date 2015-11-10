3b2ebf19e3571722c2896ddb8e8a8681f6893561
Rainer <rainer@websimple.be>
2015-11-10 17:45:49 +0100

    import initial

---
+++ lang/paquet-zencoder_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'zencoder_description' => 'Convertir vos fichier via zencoder',
+	'zencoder_nom' => 'Zencoder',
+	'zencoder_slogan' => 'Convertir vos fichier via zencoder',
+);
+
+?>
---
+++ zencoder_pipelines.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Utilisations de pipelines par Zencoder
+ *
+ * @plugin     Zencoder
+ * @copyright  2015
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Zencoder\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
+
+
+
+
+?>
